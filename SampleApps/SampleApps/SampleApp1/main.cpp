072c0672219286eb69cab9a35d90f01484a41dac
Soham Das <sodas@microsoft.com>
2025-03-18 11:00:05 -0700

    Add threadpool sample

--- SampleApps/SampleApps/SampleApp1/main.cpp
+++ SampleApps/SampleApps/SampleApp1/main.cpp
@@ -122,7 +122,7 @@ int DecryptFlow(
     return 0;
 }
 
-int main()
+int mainEncryptDecrpyt()
 {
     int choice;
     std::wstring input;
@@ -185,6 +185,74 @@ int main()
     // Wait for a key press before exiting
     std::cout << "\n\nPress any key to exit..." << std::endl;
     _getch();
+
+    return 0;
+}
+
+int mainThreadPool()
+{
+    std::wcout << L"Running sample: Taskpool..." << std::endl;
+
+    // Create app+user enclave identity
+    auto ownerId = veil::vtl0::appmodel::owner_id();
+
+    // Load enclave
+    auto flags = ENCLAVE_VBS_FLAG_DEBUG;
+
+    // Let's arbitrarily choose to spawn 3 threads
+    constexpr DWORD THREAD_COUNT = 3;
+
+    auto enclave = veil::vtl0::enclave::create(ENCLAVE_TYPE_VBS, ownerId, flags, veil::vtl0::enclave::megabytes(512));
+    veil::vtl0::enclave::load_image(enclave.get(), L"sampleenclave.dll");
+    veil::vtl0::enclave::initialize(enclave.get(), THREAD_COUNT);
+
+    // Register framework callbacks
+    veil::vtl0::enclave_api::register_callbacks(enclave.get());
+
+    // Call into enclave to 'RunTaskpoolExample' export
+    sample::args::RunTaskpoolExample data;
+    data.threadCount = THREAD_COUNT - 1;
+    THROW_IF_FAILED(veil::vtl0::enclave::call_enclave(enclave.get(), "RunTaskpoolExample", data));
+
+    std::wcout << L"Finished sample: Taskpool..." << std::endl;
+
+    // Wait for a key press before exiting
+    std::cout << "\n\nPress any key to exit..." << std::endl;
+    _getch();
+
+    return 0;
+}
+
+int main()
+{
+    int choice;
+    bool programExecuted = false;
+
+    do
+    {
+        std::cout << "\n*** Sample App Menu ***\n";
+        std::cout << "1. Encrypt, decrypt a string using enclave\n";
+        std::cout << "2. Explore executing a threadpool in the enclave\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                mainEncryptDecrpyt();
+                programExecuted = true;
+                break;
+
+            case 2:
+                mainThreadPool();
+                programExecuted = true;
+                break;
+
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
+        }
+    }
+    while (!programExecuted);
 
     return 0;
 }
