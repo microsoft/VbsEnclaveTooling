ef03b7ae6e7a877d89a2b399ac0968d5e9d3f2c9
Branden Bonaby <bbonaby@microsoft.com>
2025-04-30 21:21:06 -0700

    add initial changes

--- src/ToolingSharedLibrary/Includes/VbsEnclaveABI/Enclave/MemoryChecks.h
+++ src/ToolingSharedLibrary/Includes/VbsEnclaveABI/Enclave/MemoryChecks.h
@@ -2,7 +2,10 @@
 // Licensed under the MIT License.
 #pragma once 
 
-// __ENCLAVE_PROJECT__ must be defined inside the enclave project only.
+// __ENCLAVE_PROJECT__ must be defined inside the enclave project only. If it is defined
+// inside the host, the host won't build as winenclaveapi
+// is not compatible in an non enclave environment.
+// winenclaveapi.h is included in MemoryAllocation.h and MemoryChecks.h
 #ifdef __ENCLAVE_PROJECT__
 
 #include <VbsEnclaveABI\Shared\VbsEnclaveAbiBase.h>
--- src/ToolingSharedLibrary/Includes/VbsEnclaveABI/Enclave/Vtl0Pointers.h
+++ src/ToolingSharedLibrary/Includes/VbsEnclaveABI/Enclave/Vtl0Pointers.h
@@ -1,7 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-// __ENCLAVE_PROJECT__ must be defined inside the enclave project only.
+// __ENCLAVE_PROJECT__ must be defined inside the enclave project only. If it is defined
+// inside the host, the host won't build as winenclaveapi
+// is not compatible in an non enclave environment.
+// winenclaveapi.h is included in MemoryAllocation.h and MemoryChecks.h
 #ifdef __ENCLAVE_PROJECT__
 
 #pragma once 
