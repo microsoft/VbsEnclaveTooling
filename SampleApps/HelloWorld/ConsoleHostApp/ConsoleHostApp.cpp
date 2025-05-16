fd9b7e6dce45c9f2b8153bd036c7ed785c2a5f50
Soham Das <sodas@microsoft.com>
2025-05-16 16:06:42 -0700

    Clean ups

--- SampleApps/HelloWorld/ConsoleHostApp/ConsoleHostApp.cpp
+++ SampleApps/HelloWorld/ConsoleHostApp/ConsoleHostApp.cpp
@@ -11,12 +11,6 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-    //std::wstring input;
-    //const std::wstring encrytedKeyDirPath = std::filesystem::current_path().c_str();;
-    //const std::wstring encryptedDataDirPath = std::filesystem::current_path().c_str();
-    //std::wstring encryptedInputFilePath = encryptedDataDirPath + L"\\encrypted";
-    //std::wstring tagFilePath = encrytedKeyDirPath + L"\\tag";
-
     /******************************* Enclave setup *******************************/
 
     // Create app+user enclave identity
@@ -50,7 +44,6 @@ int main()
 
     //Call into the enclave
     auto secretResults = enclaveInterface.DoSecretMath(10, 20);
-    //std::cout << "Result = %d";
     wprintf(L"Result = %d\n", secretResults);
     wprintf(L"Press any key to exit.");
     _getch();
