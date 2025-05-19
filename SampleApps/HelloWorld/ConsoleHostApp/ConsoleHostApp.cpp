fc2cb5038327e95d84f8e2d1a17296e2e76c8044
AdamBrMSFT <adambr@microsoft.com>
2025-05-18 20:20:02 -0700

    Add Hello World sample (#76)
    * Hello World sample
    
    * edl cleanup
    
    * Clean ups
    
    * Revert "Clean ups"
    
    This reverts commit fd9b7e6dce45c9f2b8153bd036c7ed785c2a5f50.
    
    * Cleanups
    
    * Addressed PR comments
    
    ---------
    
    Co-authored-by: Soham Das <sodas@microsoft.com>

---
+++ SampleApps/HelloWorld/ConsoleHostApp/ConsoleHostApp.cpp
@@ -0,0 +1,50 @@
+// ConsoleHostApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <conio.h>
+#include <iostream>
+#include <veil\host\enclave_api.vtl0.h>
+#include <veil\host\logger.vtl0.h>
+#include <VbsEnclave\HostApp\Stubs.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    /******************************* Enclave setup *******************************/
+
+    // Create app+user enclave identity
+    auto ownerId = veil::vtl0::appmodel::owner_id();
+
+    // Load enclave
+    // We don't want DEBUG for a retail build!
+    constexpr int EnclaveCreate_Flags{
+    #ifdef _DEBUG
+        ENCLAVE_VBS_FLAG_DEBUG
+    #endif
+    };
+
+    auto flags = EnclaveCreate_Flags;
+
+    #ifndef _DEBUG
+        static_assert(flags & ENCLAVE_VBS_FLAG_DEBUG == 0, "ERROR: Do not use DEBUG flag for retail builds");
+    #endif
+
+    // Memory allocation must match enclave configuration (512mb)
+    auto enclave = veil::vtl0::enclave::create(ENCLAVE_TYPE_VBS, ownerId, flags, veil::vtl0::enclave::megabytes(512));
+    veil::vtl0::enclave::load_image(enclave.get(), L"MySecretVBSEnclave.dll");
+    veil::vtl0::enclave::initialize(enclave.get(), 1);
+
+    // Register framework callbacks
+    veil::vtl0::enclave_api::register_callbacks(enclave.get());
+
+    // Initialize enclave interface. Note that MySecretEnclave is a codegen generated class.
+    auto enclaveInterface = VbsEnclave::VTL0_Stubs::MySecretEnclave(enclave.get());
+    THROW_IF_FAILED(enclaveInterface.RegisterVtl0Callbacks());
+
+    //Call into the enclave
+    auto secretResults = enclaveInterface.DoSecretMath(10, 20);
+    wprintf(L"Result = %d\n", secretResults);
+    wprintf(L"Press any key to exit.");
+    _getch();
+}
