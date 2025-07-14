53e879785a4996d24a214744f6d548c673987969
Branden Bonaby <bbonaby@microsoft.com>
2025-07-14 11:49:03 -0700

    initial updates

--- tests/UnitTests/ToolingExecutableTests/EdlParserStructTypesTests.cpp
+++ tests/UnitTests/ToolingExecutableTests/EdlParserStructTypesTests.cpp
@@ -31,12 +31,21 @@ TEST_CLASS(EdlParserStructTypeTests)
         ParseAndValidateTestFunction(m_struct_edl_file_name, "TrustedGetStruct1", FunctionKind::Trusted, EdlTypeKind::Struct);
     }
 
+    TEST_METHOD(Parse_TrustedGetStruct1Ptr_Function)
+    {
+        ParseAndValidateTestFunction(m_struct_edl_file_name, "GetStruct1Ptr", FunctionKind::Trusted, EdlTypeKind::Struct, FunctionReturnKind::Ptr);
+    }
 
     // Untrusted functions
 
     TEST_METHOD(Parse_UntrustedGetStruct1_Function)
     {
         ParseAndValidateTestFunction(m_struct_edl_file_name, "UntrustedGetStruct1", FunctionKind::Untrusted, EdlTypeKind::Struct);
+    }
+
+    TEST_METHOD(Parse_UntrustedGetStruct1Ptr_Function)
+    {
+        ParseAndValidateTestFunction(m_struct_edl_file_name, "GetStruct1Ptr", FunctionKind::Untrusted, EdlTypeKind::Struct, FunctionReturnKind::Ptr);
     }
 };
 }
