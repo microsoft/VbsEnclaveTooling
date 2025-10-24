0eb5d8372a102f4e0101ae3c28efebc0e4b2704f
Branden Bonaby <bbonaby@microsoft.com>
2025-10-24 12:41:05 -0700

    Update based on jeff and kennys comments. No more nuget

--- rust/crates/libs/edlcodegen-tools/src/lib.rs
+++ rust/crates/libs/edlcodegen-tools/src/lib.rs
@@ -1,19 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use std::{env, path::PathBuf};
+use std::path::PathBuf;
 
-/// Returns the folder path where the tools were installed.
-fn tool_root() -> PathBuf {
-    PathBuf::from(env!("EDLCODEGEN_TOOL_PATH"))
+pub fn exes_path() -> PathBuf {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    PathBuf::from(manifest_dir).join("exes/")
 }
-
 /// Returns the full path to `edlcodegen.exe`.
 pub fn edlcodegen_path() -> PathBuf {
-    tool_root().join("bin/edlcodegen.exe")
+    exes_path().join("edlcodgen/edlcodegen.exe")
 }
 
 /// Returns the full path to `flatc.exe`.
 pub fn flatc_path() -> PathBuf {
-    tool_root().join("vcpkg/tools/flatbuffers/flatc.exe")
+    exes_path().join("flatbuffers/flatc.exe")
 }
